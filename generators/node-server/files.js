bf45a2a2f3e3da14c545f79978206d88d3d13d85
Marcelo Shima <marceloshima@gmail.com>
2025-01-27 15:24:05 -0300

    adjusts for user management

--- generators/node-server/files.js
+++ generators/node-server/files.js
@@ -9,12 +9,38 @@ export const serverFiles = {
     },
     {
       path: SERVER_NODEJS_DIR,
+      condition: ctx => ctx.generateUserManagement,
       templates: [
         'src/web/rest/user.controller.ts',
         'src/web/rest/account.controller.ts',
         'src/web/rest/public.user.controller.ts',
         'src/web/rest/management.controller.ts',
         'src/module/user.module.ts',
+        'src/security/decorators/auth-user.decorator.ts',
+        'src/migrations/1570200490072-SeedUsersRoles.ts',
+        'e2e/user.e2e-spec.ts',
+      ],
+    },
+    {
+      path: SERVER_NODEJS_DIR,
+      condition: ctx => ctx.generateBuiltInUserEntity,
+      templates: [
+        'src/domain/user.entity.ts',
+        'src/service/dto/user.dto.ts',
+        'src/service/mapper/user.mapper.ts',
+        'src/service/user.service.ts',
+      ],
+    },
+    {
+      path: SERVER_NODEJS_DIR,
+      condition: ctx => ctx.generateBuiltInAuthorityEntity,
+      templates: [
+        'src/domain/authority.entity.ts',
+      ],
+    },
+    {
+      path: SERVER_NODEJS_DIR,
+      templates: [
         'src/module/auth.module.ts',
         'src/config/application-dev.yml',
         'src/config/application-test.yml',
@@ -24,27 +50,20 @@ export const serverFiles = {
         'src/domain/base/pagination.entity.ts',
         'src/domain/base/pagination.entity.spec.ts',
         'src/domain/base/base.entity.ts',
-        'src/domain/user.entity.ts',
-        'src/domain/authority.entity.ts',
         'src/security/guards/roles.guard.ts',
         'src/security/guards/auth.guard.ts',
         'src/security/role-type.ts',
-        'src/security/decorators/auth-user.decorator.ts',
         'src/security/decorators/roles.decorator.ts',
         'src/security/index.ts',
         'src/client/header-util.ts',
         'src/client/interceptors/logging.interceptor.ts',
         'src/client/request.ts',
         'src/service/auth.service.ts',
-        'src/service/user.service.ts',
         'src/service/dto/base.dto.ts',
-        'src/service/dto/user.dto.ts',
-        'src/service/mapper/user.mapper.ts',
         'src/main.ts',
         'src/swagger.ts',
         'src/app.module.ts',
         'src/migrations/1570200270081-CreateTables.ts',
-        'src/migrations/1570200490072-SeedUsersRoles.ts',
         'src/orm.config.ts',
         'scripts/copy-resources.ts',
         'scripts/entrypoint.sh',
@@ -68,11 +87,11 @@ export const serverFiles = {
   e2e: [
     {
       path: SERVER_NODEJS_DIR,
-      templates: ['e2e/app.e2e-spec.ts', 'e2e/user.e2e-spec.ts', 'e2e/jest.e2e.config.json', 'e2e/setup.test.js'],
+      templates: ['e2e/app.e2e-spec.ts', 'e2e/jest.e2e.config.json', 'e2e/setup.test.js'],
     },
     {
       path: SERVER_NODEJS_DIR,
-      condition: generator => !generator.skipUserManagement && generator.authenticationType !== 'oauth2',
+      condition: generator => generator.generateUserManagement,
       templates: ['e2e/account.e2e-spec.ts'],
     },
   ],
@@ -84,7 +103,7 @@ export const serverFiles = {
   jwt: [
     {
       path: SERVER_NODEJS_DIR,
-      condition: generator => generator.authenticationType === 'jwt',
+      condition: ctx => ctx.authenticationTypeJwt && ctx.generateUserManagement,
       templates: [
         'src/web/rest/user.jwt.controller.ts',
         'src/security/password-util.ts',
@@ -98,7 +117,7 @@ export const serverFiles = {
   oauth2: [
     {
       path: SERVER_NODEJS_DIR,
-      condition: generator => generator.authenticationType === 'oauth2',
+      condition: generator => generator.authenticationTypeOauth2,
       templates: ['src/web/rest/user.oauth2.controller.ts', 'src/security/passport.oauth2.strategy.ts', 'src/security/oauth2.config.ts'],
     },
   ],
